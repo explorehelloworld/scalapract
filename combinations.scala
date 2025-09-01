2b728a76434958977fd3e185f752c2027d9672ad
Explorehelloworld <36978810+angularprojects4all@users.noreply.github.com>
2025-09-01 22:54:56 +0530

    Update combinations.scala

--- combinations.scala
+++ combinations.scala
@@ -9,3 +9,6 @@ val combinations = generateCombinations(elements)
 println(combinations)
 //output: List(List(4), List(3), List(3, 4), List(2), List(2, 4), List(2, 3), List(2, 3, 4), 
 //..contd >>> List(1), List(1, 4), List(1, 3), List(1, 3, 4), List(1, 2), List(1, 2, 4), List(1, 2, 3), List(1, 2, 3, 4))
+
+
+//its only powerset, combinations is subset of powerset. if i need to draw r from n combinations nCr = n!/(r!)(n-r!)
